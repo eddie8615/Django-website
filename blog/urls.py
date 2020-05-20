8faf8ade1ef00d5fa7714632bef9b19d1c919d04
cxk858 <cxk858@cs.bham.ac.uk>
2020-05-21 00:52:21 +0900

    Adding and editing posts components are added

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
+from . import views1
+from .forms import PostForm
 
 urlpatterns=[
-    path('', views.postlists),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('', views1.postlists),
+    path('post/<int:pk>/', views1.post_detail, name='post_detail'),
+    path('post/new/', views1.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views1.post_edit, name='post_edit'),
     # path(r'contact/', views.contact, name='contacts'),
     # path(r'about/', views.about, name='about')
 
